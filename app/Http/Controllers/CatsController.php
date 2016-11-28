37dfe80b00e12699023e4123b89ef0b3e5c4eb48
PC Hater <p.chater@icloud.com>
2016-11-28 17:56:47 +0000

    Instantiated the CatsController. Fleshed it out also, updated routes. Will add _notes dir and any .swp files to .gitignore.

---
+++ app/Http/Controllers/CatsController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\CatsController;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Cats
+
+class CatsController extends Controller
+{
+	public function __construct() {
+		/*
+			List all Categories and Children
+		*/
+	}
+	public function create() {
+		/*
+			Create a new Category
+		*/
+	}
+
+	public function update($id) {
+		/*
+			Update existing category
+		*/
+	}
+
+	public function remove($id) {
+		/* 
+			TODO: Insert Awesome Kill Cat Command.
+			NOTE: No Cats were actually killed in building this application.
+			Remove a category
+		*/
+	}
+}
