726efbe2100617f98948eeb817360852b3fd914f
PC Hater <p.chater@icloud.com>
2016-11-29 20:06:29 +0000

    Gave the forms some pretty default responsive Bootstrap styling

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -8,18 +8,18 @@
     {!! Form::open(['url' => 'categories']) !!}
         <!-- Give the category a name. -->
         {!! Form::label('title','Title:') !!}
-        {!! Form::text('title') !!}
+        {!! Form::text('title', '', ['class' => 'form-control']) !!}
         <br />
         <!-- Give a good description. -->
         {!! Form::label('description', 'Description:') !!}
-        {!! Form::textarea('description') !!}
+        {!! Form::textarea('description', '', ['class' => 'form-control']) !!}
         <br />
         <!-- Provide the slug. A slug is what the browser will read for SEO. -->
         {!! Form::label('slug','Slug:') !!}
-        {!! Form::text('slug') !!}
+        {!! Form::text('slug', '', ['class' => 'form-control']) !!}
         <br />
         <!-- Submit the form -->
-        {!! Form::submit('Add Category') !!}
-    <!-- Close the form -->    
+        {!! Form::submit('Add Category', ['class' => 'btn btn-success']) !!}
+    <!-- Close the form -->
     {!! Form::close() !!}
 @stop
