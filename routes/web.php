593276b42cc5a5e9f45747d981c28ee434b178ad
PC Hater <p.chater@icloud.com>
2016-11-30 02:51:23 +0000

    Cleaned up the Web Routes to be RESTful.

--- routes/web.php
+++ routes/web.php
@@ -15,21 +15,23 @@ Route::get('/', 'CategoriesController@index');
 /*
  * This collection of Routes handles the Category functionality.
  */
+//
+// // Show all categories
+// Route::get('categories', 'CategoriesController@index');
+// // Create a category
+// Route::get('categories/create', 'CategoriesController@create');
+// // Show a category by a given ID.
+// Route::get('categories/{id}', 'CategoriesController@show');
+// // Method for storing the data after input.
+// Route::post('categories', 'CategoriesController@store');
+Route::resource('categories', 'CategoriesController');
 
-// Show all categories
-Route::get('categories', 'CategoriesController@index');
-// Create a category
-Route::get('categories/create', 'CategoriesController@create');
-// Show a category by a given ID.
-Route::get('categories/{id}', 'CategoriesController@show');
-// Method for storing the data after input.
-Route::post('categories', 'CategoriesController@store');
-
-// Show all products
-Route::get('products','ProductsController@index');
-// Create a products
-Route::get('products/create','ProductsController@create');
-// Show a Product by a Given ID.
-Route::get('products/{id}', 'ProductsController@show');
-// Store the data after input
-Route::post('products', 'ProductsController@store');
+// // Show all products
+// Route::get('products','ProductsController@index');
+// // Create a products
+// Route::get('products/create','ProductsController@create');
+// // Show a Product by a Given ID.
+// Route::get('products/{id}', 'ProductsController@show');
+// // Store the data after input
+// Route::post('products', 'ProductsController@store');
+Route::resource('products', 'ProductsController');
